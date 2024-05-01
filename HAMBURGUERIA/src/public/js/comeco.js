944cff3a3bde826876881153bf6ec93e36f002f9
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2024-04-30 22:16:55 -0300

    Add files via upload

---
+++ HAMBURGUERIA/src/public/js/comeco.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('#telaComeco').addEventListener('click', () => {
+    document.querySelector('#telaComeco').classList.add("subir")
+    document.querySelector('.comecar').classList.add("subir")
+})
