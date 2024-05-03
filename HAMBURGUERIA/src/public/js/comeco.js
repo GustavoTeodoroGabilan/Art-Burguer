01b1c0b63c482641477ac0e92161ee8270a21c51
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2024-05-02 21:46:04 -0300

    Add files via upload

--- HAMBURGUERIA/src/public/js/comeco.js
+++ HAMBURGUERIA/src/public/js/comeco.js
@@ -3,3 +3,43 @@
 document.querySelector('#telaComeco').addEventListener('click', () => {
     document.querySelector('#telaComeco').classList.add("subir")
 })
+
+document.querySelector('#promocoes').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.add("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer") 
+})
+
+document.querySelector('#bebidas').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.add("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer") 
+})
+
+document.querySelector('#acompanhamentos').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.add("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer") 
+})
+
+document.querySelector('#sobremesas').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.add("crescer")
+    document.querySelector('#hamburguers').classList.remove("crescer") 
+})
+
+document.querySelector('#hamburguers').addEventListener('click', () => {
+    document.querySelector('#promocoes').classList.remove("crescer")
+    document.querySelector('#bebidas').classList.remove("crescer")
+    document.querySelector('#acompanhamentos').classList.remove("crescer")
+    document.querySelector('#sobremesas').classList.remove("crescer")
+    document.querySelector('#hamburguers').classList.add("crescer") 
+})
