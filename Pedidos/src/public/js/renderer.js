87d5c34e17282abc8480919e4cbb93736c8e3a0c
gabrielrodriguesdossantos <115747310+gabrielrodriguesdossantos@users.noreply.github.com>
2024-06-20 22:02:43 -0300

    Add files via upload

--- Pedidos/src/public/js/renderer.js
+++ Pedidos/src/public/js/renderer.js
@@ -24,3 +24,61 @@ function renderizarPedidos(lanche) {
 ;
     });
   }
+
+// Seleciona os elementos HTML relevantes
+const pedidosProntos = document.querySelector('.pedidosProntos');
+const pedidosPreparando = document.querySelector('#preparando');
+
+// Array para armazenar os nomes dos pedidos prontos
+let nomesPedidosProntos = [];
+
+function moverPedidoParaPronto() {
+    // Verifica se há pedidos na seção "Preparando"
+    if (pedidosPreparando.children.length > 0) {
+        // Move o primeiro pedido da seção "Preparando" para "Pronto"
+        const primeiroPedido = pedidosPreparando.firstElementChild;
+        const nomePedido = primeiroPedido.innerText.trim();
+
+        // Remove o pedido da seção "Preparando"
+        pedidosPreparando.removeChild(primeiroPedido);
+
+        // Adiciona o nome do pedido ao array de pedidos prontos
+        nomesPedidosProntos.push(nomePedido);
+
+        // Cria um novo elemento para representar o pedido na seção "Pronto"
+        const novoPedidoPronto = document.createElement('div');
+        novoPedidoPronto.classList.add('pedidoPronto');
+        novoPedidoPronto.innerHTML = `<h2>${nomePedido}</h2>`;
+
+        // Adiciona o novo pedido na seção "Pronto"
+        pedidosProntos.appendChild(novoPedidoPronto);
+
+        // Define um timeout para remover o primeiro pedido após 10 segundos
+        setTimeout(() => {
+            // Verifica se ainda há pedidos prontos e se o nome do primeiro pedido é igual ao nomePedido
+            if (nomesPedidosProntos.length > 0 && nomesPedidosProntos[0] === nomePedido) {
+                // Remove o primeiro pedido da seção "Pronto"
+                pedidosProntos.removeChild(pedidosProntos.firstElementChild);
+                // Remove o primeiro nome do array de pedidos prontos
+                nomesPedidosProntos.shift();
+            }
+        }, 30000); // 10000 milissegundos = 10 segundos
+    }
+}
+
+// Simula a mudança de status a cada 5 segundos
+setInterval(moverPedidoParaPronto, 5000);
+
+// Função para adicionar um novo pedido à seção "Preparando"
+function adicionarNovoPedido(nomePedido) {
+    // Cria um novo elemento para representar o pedido na seção "Preparando"
+    const novoPedido = document.createElement('div');
+    novoPedido.classList.add('pedido');
+    novoPedido.innerHTML = `<h2>${nomePedido}</h2>`;
+
+    // Adiciona o novo pedido na seção "Preparando"
+    pedidosPreparando.appendChild(novoPedido);
+}
+
+// Exemplo de adição de novo pedido (pode ser chamado ao adicionar um cliente)
+adicionarNovoPedido('Novo Cliente');
